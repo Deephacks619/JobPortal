c6bab6395340e9fd9c34c52e85492e512b8fbac1
deephacks619 <deepakkyatham@gmai.com>
2020-10-19 23:24:15 +0530

    Updates

---
+++ precache-manifest.a42ae1a22feaf2708ab2534602ee8057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed204170716a23b9031480305758eb3",
+    "url": "/Deephacks619/JobPortal/index.html"
+  },
+  {
+    "revision": "49e61d95538030563267",
+    "url": "/Deephacks619/JobPortal/static/css/main.ef1dad0c.chunk.css"
+  },
+  {
+    "revision": "1f281bc725cf61653f48",
+    "url": "/Deephacks619/JobPortal/static/js/2.47dad7d4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Deephacks619/JobPortal/static/js/2.47dad7d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e61d95538030563267",
+    "url": "/Deephacks619/JobPortal/static/js/main.8fcdfd2b.chunk.js"
+  },
+  {
+    "revision": "83eeb846a0a86229c61b",
+    "url": "/Deephacks619/JobPortal/static/js/runtime-main.fe32206b.js"
+  }
+]);
